2c65f761350f0318aebe06bd61dc0588238aedff
Kağan Efe <efekaan267@gmail.com>
2024-02-18 20:13:11 +0300

    First Commit
    Kurgunun ve Mimarinin Oluşturulması.

---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,52 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using Persistence.Contexts;
+using System.Linq.Expressions;
+
+namespace Persistence.Repositories;
+
+public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+{
+    public BrandRepository(BaseDbContext context) : base(context)
+    {
+    }
+
+    public Task<Brand> AddAsync(Brand entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Brand> DeleteAsync(Brand entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Brand?> GetAsync(Expression<Func<Brand, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IPaginate<Brand>> GetListAsync(Expression<Func<Brand, bool>>? predicate = null, Func<IQueryable<Brand>, IOrderedQueryable<Brand>>? orderBy = null, Func<IQueryable<Brand>, IIncludableQueryable<Brand, object>>? include = null, int index = 0, int size = 10, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IPaginate<Brand>> GetListByDynamicAsync(Dynamic dynamic, Func<IQueryable<Brand>, IIncludableQueryable<Brand, object>>? include = null, int index = 0, int size = 10, bool enableTracking = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Brand> UpdateAsync(Brand entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    IQueryable<Brand> IQuery<Brand>.Query()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -0,0 +1,26 @@
+using Persistence;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//builder.Services.AddApplicationServices();
+//builder.Services.AddSecurityServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddInfrastructureServices();
+//builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
